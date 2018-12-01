eeb8509cc6f982f988dce62179060f82f80a9fd3
Peppy Sisay <peppysisay@gmail.com>
2018-12-01 09:49:59 -0800

    Update website

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const pxtorem = require('postcss-pxtorem')
 
 module.exports = {
   siteMetadata: {
-    url: 'https://peppys.github.io',
+    url: 'https://www.peppysisay.com',
     title: 'Peppy\'s Blog',
     subtitle:
       'Hey 👋🏾 I\'m a young black software engineer, striving for excellence! Currently a back-end engineer at <a href="https://shipwithbolt.com">Bolt</a>',
